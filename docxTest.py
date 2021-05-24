4acae5473cb36c7af562494e7cf85d262c237183
brettandrews3 <brettandrews3@gmail.com>
2021-05-24 17:24:45 -0500

    Initial code written. Debugging is next, then commenting throughout.

--- docxTest.py
+++ docxTest.py
@@ -35,3 +35,13 @@ hdr_cells = table.rows[0].cells
 hdr_cells[0].text = 'Qty'
 hdr_cells[1].text = 'Id'
 hdr_cells[2].text = 'Desc'
+
+for qty, id, desc in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = str(qty)
+    row_cells[1].text = id
+    row_cells[2].text = desc
+
+document.add_page_break()
+
+document.save('demoDoc.docx')
